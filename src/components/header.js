0429987912055b6f0cb8932b765b64e3bea018cb
Victor Garcia <vgc1902@gmail.com>
2021-03-02 16:47:14 +0100

    feat(home): add social links on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Logo from "../assets/img/logo-jcf.png"
+import FacebookLogo from "../assets/img/facebook.svg"
+import InstagramLogo from "../assets/img/instagram.svg"
 
 export const Header = ({ title }) => {
   const {
@@ -24,11 +26,21 @@ export const Header = ({ title }) => {
         <img src={Logo} alt="Junta Central Fallera" />
       </Link>
       <nav>
-        {menu.map(({ node: { label, url, id } }) => (
-          <Link key={id} to={url}>
-            {label}
-          </Link>
-        ))}
+        <div className="nav-social">
+          <a href="https://www.facebook.com/Falla.Castellon" target="_blank">
+            <img src={FacebookLogo} alt="Facebook" />
+          </a>
+          <a href="https://www.instagram.com/fallacastellon/" target="_blank">
+            <img src={InstagramLogo} alt="Facebook" />
+          </a>
+        </div>
+        <div class="nav-menu">
+          {menu.map(({ node: { label, url, id } }) => (
+            <Link key={id} to={url}>
+              {label}
+            </Link>
+          ))}
+        </div>
       </nav>
     </header>
   )
